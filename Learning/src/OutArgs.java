86ceceb76c35a62d8a70ed947073599ef2ccd4b6
Aliaksandr Taranishyn <Aliaksandr_Taranishyn@epam.com>
2019-05-10 17:43:29 +0300

    java

--- Learning/src/OutArgs.java
+++
@@ -1,6 +0,0 @@
-public class OutArgs {
-    public static void main(String[] args) {
-        for (String str : args)
-            System.out.printf("Aprj-> %s%n", str);
-    }
-}
--- Learning/src/Runner.java
+++
@@ -1,9 +0,0 @@
-package Learning;
-
-public class Runner {
-    public static void main(String[] args) {
-        Runtime rt = Runtime.getRuntime();
-        System.out.println("Full memory value: " + rt.totalMemory());
-    }
-
-}
